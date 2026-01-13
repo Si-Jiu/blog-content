dd11d7ab2d825ec9219a747403518e93c0a06165
肆鸠 <si_jiu49@outlook.com>
2026-01-13 18:38:19 +0800

    refactor device categories and update device details

--- data/devices.ts
+++ data/devices.ts
@@ -16,22 +16,32 @@ export type DeviceCategory = {
 };
 
 export const devicesData: DeviceCategory = {
-	Apple: [
+	Phone: [
 		{
-			name: "iPad（第 9 代)",
+			name: "iPhone 7 Plus",
+			image:
+				"https://www.apple.com.cn/v/iphone/compare/ak/images/overview/compare_iphone7plus_black__ejjh56vz0i2q_large.jpg",
+			specs: "黑色 / 128GB",
+			description: "日常用，使用 iOS15，日用不卡（吧），可打三角洲",
+			link: "https://support.apple.com/zh-cn/111953",
+		},
+	],
+	Tablet: [
+		{
+			name: "iPad (第 9 代)",
 			image:
 				"https://www.apple.com.cn/v/ipad/compare/ak/images/overview/ipad_9th_gen_space_gray__bmh62f2hq9iu_large.jpg",
 			specs: "深空灰色 / 256GB",
-			description: "拿来上学用的，A13完全够轻度游戏+日常使用了",
+			description: "上学用，使用 iPadOS26，可打三角洲和各种 2D 二游",
 			link: "https://support.apple.com/zh-cn/111898",
 		},
 	],
-	Dell: [
+	Laptop: [
 		{
 			name: "Dell Inspiron 14 5410",
 			image: "https://i.imgur.com/rjiPrOB.png",
-			specs: "512GB / 16G DDR4 3200MHz / i5-11300H",
-			description: "轻度游戏+学习完全够了",
+			specs: "512GB / 16G DDR4 3200MHz / i5-11300H / MX 450 2GB",
+			description: "日常用，使用 Arch Linux，打老游戏还可以",
 			link: "https://www.dell.com/zh-hk/shop/dell-%E7%AD%86%E8%A8%98%E5%9E%8B%E9%9B%BB%E8%85%A6/inspiron-14-%E7%AD%86%E8%A8%98%E5%9E%8B%E9%9B%BB%E8%85%A6/spd/inspiron-14-5410-laptop",
 		},
 	],
